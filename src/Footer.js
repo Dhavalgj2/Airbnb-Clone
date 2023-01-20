3a727ec27861dbff63420022434562ee6c40b101
Dhaval <71810466+Dhavalgj2@users.noreply.github.com>
2023-01-20 16:39:17 +1100

    footer changes

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+      <p>© 2020 Airbnb clone! Developed By Dhaval</p>
       <p>Privacy · Terms · Sitemap · Company Details</p>
     </div>
   );
